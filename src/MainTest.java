4b9c8f6df49354eee4dd9c5f5adeaa8a72e0524d
galgavrilenko <ggalkka@gmail.com>
2024-07-18 01:33:23 +0300

    less1

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,11 +1,44 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest extends CoreTestCase
 {
-
+    @Before
+    public void TestStartTest()
+    {
+    System.out.println("Start test");
+    }
+    @After
+    public void TestFinishTest()
+    {
+        System.out.println("Finish test");
+    }
     @Test
-    public void myFirstTest(){
+    public void myFirstTest()
+    {
+//        Assert.fail("This message will by printed"); /*ложно позитивное срабатывание*/
+//        this.assertFail();
+        System.out.println("First test");
+        int expected = 20;
+        int actual = 5*5;
 
+//        if(actual != expected){
+//            Assert.fail("Ошибка");
+//        }
+//        или
+        Assert.assertTrue( "5*5* !=5", actual == expected);
     }
 
+//    private void assertFail()
+//    {
+//        Assert.fail("This message will by printed"); /*ложно позитивное срабатывание*/
+//    }
+
+    @Test
+    public void mySecondTest()
+    {
+        System.out.println("Second test");
+    }
 }
