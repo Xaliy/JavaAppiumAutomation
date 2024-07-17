f2bcfdb00b29d2e136b287b350ffcf4babda74db
galgavrilenko <ggalkka@gmail.com>
2024-07-18 01:00:18 +0300

    master new less1

--- src/MainTest.java
+++ src/MainTest.java
@@ -2,24 +2,10 @@ import org.junit.Test;
 
 public class MainTest extends CoreTestCase
 {
-    public void typeStartMessage(){
-        /*super  - обозначает , что сначала выполняем весь код в классе родителе
-        и только потом выполняем в текущем классе!
-         если убрать super, то будет выводиться только то что в текущем классе,
-         т.е метод внутри текущего класса стал полностью переопределен
-         */
-        super.typeStartMessage(); /* если это убрать, то метод typeStartMessage будет переопределен */
-        System.out.println("Current class is MainTest");
-    }
 
     @Test
     public void myFirstTest(){
         this.typeStartMessage();
     }
 
-    @Test
-    public void mySecondTest(){
-        this.typeStartMessage();
-
-    }
 }
