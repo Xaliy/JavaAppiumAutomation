a51468aa46a7c211c7dbeb61bb9367ca0a638eca
galgavrilenko <ggalkka@gmail.com>
2024-07-20 13:23:39 +0300

    less1 2 задание

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,17 @@
 public class MainClass {
 
+    private int class_number =20;
+
     public int getLocalNumber() {
         /*** метод возвращает 14 ***/
         int localNumber = 14;
 //        System.out.println("the value of localNumber = " + localNumber);
         return localNumber;
+    }
+
+    public int getClassNumber(){
+        /*** метод возвращает приватную переменную  class_number 20***/
+//        System.out.println("the value of class_number = " + this.class_number);
+        return class_number;
     }
 }
