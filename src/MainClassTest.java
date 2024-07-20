46f2a53a93a0f9ab1ac902b530ac7b17357b11c6
galgavrilenko <ggalkka@gmail.com>
2024-07-20 13:33:22 +0300

    урок 1 задание 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -5,20 +5,13 @@ public class MainClassTest extends MainClass  {
 
     @Test
     public void testGetLocalNumber() {
-<<<<<<< HEAD
-        if (getLocalNumber() == 14 )
-            System.out.println(" getLocalNumber works correctly " +  getLocalNumber());
-=======
         /*** тест  проверяет, что метод getLocalNumber возвращает число 14 ***/
         if (getLocalNumber() == 14 )
             System.out.println("getLocalNumber works correctly " +  getLocalNumber());
->>>>>>> less2
         else
             System.out.println("getLocalNumber does not work correctly " +  getLocalNumber());
     }
 
-<<<<<<< HEAD
-=======
     @Test
     public void testGetClassNumber(){
         /*** тест  проверяет, что метод getClassNumber возвращает число больше 45 ***/
@@ -27,5 +20,4 @@ public class MainClassTest extends MainClass  {
         else
             System.out.println("getClassNumber does not work correctly " +  getClassNumber());
     }
->>>>>>> less2
 }
