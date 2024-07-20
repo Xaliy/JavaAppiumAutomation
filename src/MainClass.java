e60b2dc85fb9f59fe9ed229c83f89d1d300b9788
galgavrilenko <ggalkka@gmail.com>
2024-07-20 12:50:07 +0300

    test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MainClass {
+public class MainClass {
+
+    public int getLocalNumber() {
+        /*** метод возвращает 14 ***/
+        int localNumber = 14;
+//        System.out.println("the value of localNumber = " + localNumber);
+        return localNumber;
+    }
+
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class MainClassTest {
+import org.junit.Test;
+
+
+public class MainClassTest extends MainClass  {
+
+    @Test
+    public void testGetLocalNumber() {
+        if (getLocalNumber() == 14 )
+            System.out.println(" getLocalNumber works correctly " +  getLocalNumber());
+        else
+            System.out.println("getLocalNumber does not work correctly " +  getLocalNumber());
+    }
+
 }
